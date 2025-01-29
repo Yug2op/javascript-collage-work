46ec6dc4bfe2f8ecad6a57cbf16fd053e8590725
Yugank Tripathi <mr.yugank.2000@gmail.com>
2025-01-29 13:43:55 +0530

    Refactor Palindrom.js to uncomment and enhance palindromic number functionality

--- Palindrom.js
+++ Palindrom.js
@@ -1,20 +1,10 @@
 // palindrom
 
-// function palindrom(str) {
-//   let reversed = str.split('').reverse().join('');
-//   return str === reversed;
-// }
+function palindrom(str) {
+  let reversed = str.split('').reverse().join('');
+  return str === reversed;
+}
 
 
-// console.log( palindrom("hello")) // false
-// console.log(palindrom("racecar")) // true
-
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let palindromicNumbers = arr.filter(num => {
-    let str = num.toString();
-    return str === str.split('').reverse().join('');
-});
-
-console.log(palindromicNumbers); // [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log( palindrom("hello")) // false
+console.log(palindrom("racecar")) // true
